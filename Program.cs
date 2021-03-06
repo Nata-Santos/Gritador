10ec3d3d2e0e3ee07d599652dc8edf4aac3d4870
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-05 23:23:09 -0300

    Entrega da atividade Gritador

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string grito;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Digite o que pensa e será exibido como grito");
+            Console.WriteLine("--------------------------------------------");
+            Console.ResetColor();
+            
+            Console.WriteLine();
+            grito = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"{grito.ToUpper()}");
+
+
+        
+        }
+    }
+}
